839f3d163f40825b3d04361914f7f2f59bbfb8e5
xenophy <kotsutsumi@xenophy.com>
2016-02-06 01:01:06 +0900

    update

---
+++ ext6/loginform4/codex/login/app/Application.js
@@ -0,0 +1,70 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Login.Application
+
+/**
+ * Login.Application
+ *
+ * Copyright (c) 2016 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+Ext.define('Login.Application', {
+
+    // {{{ extend
+
+    extend: 'Ext.app.Application',
+
+    // }}}
+    // {{{ name
+
+    name: 'Login',
+
+    // }}}
+    // {{{ requires
+
+    requires: [
+        'Ext.direct.*'
+    ],
+
+    // }}}
+    // {{{ stores
+
+    stores: [
+        // TODO: add global / shared stores here
+    ],
+
+    // }}}
+    // {{{ launch
+
+    launch: function () {
+
+    },
+
+    // }}}
+    // {{{ onAppUpdate
+
+    onAppUpdate: function () {
+
+        Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
+            function (choice) {
+                if (choice === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
--- setup/ext601/loginform4.sh
+++ setup/ext601/loginform4.sh
@@ -10,16 +10,26 @@ sencha -sdk ~/Library/Sencha/ext-6.0.1/ generate app Login ./login
 rm app.json
 rm login/app/view/main/Main*.js
 rm login/classic/src/view/main/*.js
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app/view/main/MainController.js > login/app/view/main/MainController.js
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Form.js > login/classic/src/view/main/Form.js
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Main.js > login/classic/src/view/main/Main.js
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app.json > login/app.json
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/sass/src/view/main/Main.scss > classic/sass/src/view/main/Main.scss
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/app/view/main/MainController.js > login/app/view/main/MainController.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/classic/src/view/main/Form.js > login/classic/src/view/main/Form.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/classic/src/view/main/Main.js > login/classic/src/view/main/Main.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/app.json > login/app.json
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/classic/sass/src/view/main/Main.scss > login/classic/sass/src/view/main/Main.scss
 mkdir -p login/resources/images
-curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/resources/images/logo.png > login/resources/images/logo.png
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/resources/images/logo.png > login/resources/images/logo.png
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/fuel/app/config/routes.php > ../fuel/app/config/routes.php
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/fuel/app/config/auth.php > ../fuel/app/config/auth.php
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/fuel/app/config/config.php > ../fuel/app/config/config.php
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/fuel/app/classes/controller/welcome.php > ../fuel/app/classes/controller/welcome.php
+
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/front/build.xml > front/build.xml
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform4/codex/login/build.xml > login/build.xml
+
+mkdir -p ../fuel/app/views/welcome/login/
 mv front/index.html front/index.php
 mv login/index.html login/index.php
 sed -i -e "s/RewriteBase \/RewriteBase \/~$NAME\//" ../public_html/.htaccess
+sed -i -e "s/RewriteCond %\{REQUEST_FILENAME\} !-d/#RewriteCond %\{REQUEST_FILENAME\} !-d/" ../public_html/.htaccess
 cd login
 sencha app build
 
