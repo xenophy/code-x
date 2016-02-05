1f99a7048be3f4405ecabee6fc07f2ce85c46f1b
xenophy <kotsutsumi@xenophy.com>
2016-02-05 14:04:06 +0900

    update

--- setup/ext601/loginform3.sh
+++ setup/ext601/loginform3.sh
@@ -6,12 +6,13 @@ cd $NAME
 sencha generate workspace ./
 sencha -sdk ~/Library/Sencha/ext-6.0.1/ generate app Front ./front
 sencha -sdk ~/Library/Sencha/ext-6.0.1/ generate app Login ./login
+rm app.json
 rm login/app/view/main/Main*.js
 rm login/classic/src/view/main/*.js
-
-
-
-
-
-
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app/view/main/MainController.js > login/app/view/main/MainController.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Form.js > login/classic/src/view/main/Form.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Main.js > login/classic/src/view/main/Main.js
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app.json > login/app.json
+cd login
+sencha app watch
 
