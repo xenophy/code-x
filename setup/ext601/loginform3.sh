77cb7d75e00be708082817e1d35a2ac92dc339d9
xenophy <kotsutsumi@xenophy.com>
2016-02-05 14:17:13 +0900

    update

--- setup/ext601/loginform3.sh
+++ setup/ext601/loginform3.sh
@@ -1,6 +1,7 @@
 NAME=$1
 oil create $NAME
 cd $NAME
+mv public public_html
 mkdir $NAME
 cd $NAME
 sencha generate workspace ./
@@ -13,6 +14,10 @@ curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app
 curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Form.js > login/classic/src/view/main/Form.js
 curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/classic/src/view/main/Main.js > login/classic/src/view/main/Main.js
 curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/app.json > login/app.json
+mkdir -p login/resources/images
+curl https://raw.githubusercontent.com/xenophy/code-x/master/ext6/loginform3/resources/images/logo.png > login/resources/images/logo.png
+mv front/index.html front/index.php
+mv login/index.html login/index.php
 cd login
 sencha app watch
 
